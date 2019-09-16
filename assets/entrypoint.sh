466da84262ccf5808e22b57718af83bd785851a5
Jan Boonen <jan.boonen@geodan.nl>
2019-09-16 22:49:07 +0200

    Added simple data directory and templated most important settings.

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -2,6 +2,16 @@
 
 /usr/local/bin/confd -onetime -backend env
 
+# Initialize JDBCConfig module for first time
+if [ ! -f ${GEOSERVER_DATA_DIR}/jdbcconfig/jdbcconfig.properties ]; then 
+    cp ${GEOSERVER_DATA_DIR}/jdbcconfig/jdbcconfig_init.properties ${GEOSERVER_DATA_DIR}/jdbcconfig/jdbcconfig.properties 
+fi
+
+# test if we already configured this container, only configure it once
+if [ ! -f ${CATALINA_HOME}/conf/default_config_set ]; then
+    bash /opt/geoserver_config.sh &
+fi
+
 # Make sure all fonts in volumes are available
 fc-cache -f -v
 
